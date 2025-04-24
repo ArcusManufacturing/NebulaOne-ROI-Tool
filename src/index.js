65aaa146719bed39a9ba9b0a02c4862f0e82004e
Arcus Manufacturing <166059499+ArcusManufacturing@users.noreply.github.com>
2025-04-23 22:34:08 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NebulaOneROICalculator from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <NebulaOneROICalculator />
+  </React.StrictMode>
+);
